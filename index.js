4bed67ac1aa1da82ab2e8627638b673bc025c96a
pablopunk <pablovarela182@gmail.com>
2018-09-20 22:29:08 +0200

    Refactor into lib/

--- index.js
+++ index.js
@@ -1,58 +1,18 @@
-const { MongoClient } = require('mongodb')
+const { getClient, getAllCollectionsFromDb, getResourceFromDb } = require('./lib/mongo')
+const { getResourceFromUrl, convertAllCollectionsToNamesArray } = require('./lib/string')
+const { setResponseHeaders } = require('./lib/server')
+
 require('dotenv').config()
 
-// Globals
 const { env } = process
-let db
-
-// DB Utils
-const convertAllCollectionsToNamesArray = (collections) => {
-  collections.shift()
-
-  return collections.map(c => c.name)
-}
-const getDatabaseNameFromUrl = (url) => {
-  const parts = url.split('/')
-
-  return parts[parts.length - 1]
-}
-const getAuthenticatedMongoUrl = (url, { user, pass }) => {
-  const protocol = 'mongodb://'
-  const urlWithoutProtocol = url.replace(protocol, '')
-
-  return `${protocol}${user}:${pass}@${urlWithoutProtocol}`
-}
-const getResourceFromUrl = path => path.split('/')[1]
-const getResourceFromDb = async (resource, db) => {
-  const collection = await db.collection(resource)
-  const results = await collection.find({}).toArray()
-
-  return results || []
-}
-const getAllCollectionsFromDb = async (db) => db.listCollections().toArray()
-
-// Server utils
 const headers = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Accept-Laguage, User-Agent'
 }
-const setResponseHeaders = (headers, res) => {
-  for (const h in headers) {
-    if (Object.prototype.hasOwnProperty.call(headers, h)) {
-      res.setHeader(h, headers[h])
-    }
-  }
-  return res
-}
 
 module.exports = async (req, res) => {
-  if (!db) {
-    const url = getAuthenticatedMongoUrl(env.DB_URL, { user: env.DB_USER, pass: env.DB_PASS })
-    const client = await MongoClient.connect(url)
-    const dbName = getDatabaseNameFromUrl(url)
-
-    db = client.db(dbName)
-  }
+  const db = await getClient(env.DB_URL,
+    { user: env.DB_USER, pass: env.DB_PASS })
 
   res = setResponseHeaders(headers, res)
   const { url } = req
